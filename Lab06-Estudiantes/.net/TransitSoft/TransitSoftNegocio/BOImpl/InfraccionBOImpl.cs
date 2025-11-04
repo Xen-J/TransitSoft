deb44ea10eb5f2e5b11d0b7caa385b22f0ef6882
xen <xendra.jr@gmail.com>
2025-11-04 10:56:47 -0500

    Archivos del lab6

---
+++ Lab06-Estudiantes/.net/TransitSoft/TransitSoftNegocio/BOImpl/InfraccionBOImpl.cs
@@ -0,0 +1,25 @@
+﻿using PUCP.TransitSoft.Modelo;
+using PUCP.TransitSoft.Negocio.BO;
+using PUCP.TransitSoft.Persistencia.DAO;
+using PUCP.TransitSoft.Persistencia.DAOImpl;
+using System.Collections.Generic;
+
+namespace PUCP.TransitSoft.Negocio.BOImpl {
+    public class InfraccionBOImpl : IInfraccionBO {
+        private readonly IInfraccionDAO infraccionDao;
+
+        public InfraccionBOImpl() { 
+            this.infraccionDao = new InfraccionDAOImpl();
+        }
+
+        public List<Infraccion> Listar() {
+            IInfraccionDAO dao = new InfraccionDAOImpl();
+            return dao.LeerTodos();
+        }
+
+        public int Crear(Infraccion modelo) {
+            IInfraccionDAO dao = new InfraccionDAOImpl();
+            return dao.Crear(modelo);
+        }
+    }
+}
